014d8f634f555b5e0f761bb3b24f58e0731225cc
Narazaka <info@narazaka.net>
2025-01-24 07:51:36 +0900

    lang / desc

--- Editor/AvatarParameterExclusiveGroupEditor.cs
+++ Editor/AvatarParameterExclusiveGroupEditor.cs
@@ -1,4 +1,4 @@
-﻿using UnityEditor;
+using UnityEditor;
 using UnityEditorInternal;
 using net.narazaka.vrchat.avatar_parameters_driver;
 using Narazaka.VRChat.AvatarParametersUtil.Editor;
@@ -12,6 +12,8 @@ namespace net.narazaka.vrchat.avatar_parameters_exclusive_group.editor
         SerializedProperty ExclusiveParametersProperty;
         ReorderableList ExclusiveParametersList;
 
+        bool ShowDetail = false;
+
         void OnEnable()
         {
             AvatarParametersUtilEditor.Get(serializedObject, true);
@@ -21,7 +23,7 @@ namespace net.narazaka.vrchat.avatar_parameters_exclusive_group.editor
             {
                 drawHeaderCallback = (rect) =>
                 {
-                    EditorGUI.LabelField(rect, "Exclusive Parameters");
+                    EditorGUI.LabelField(rect, T.ExclusiveParameters);
                 },
                 drawElementCallback = (rect, index, isActive, isFocused) =>
                 {
@@ -31,18 +33,18 @@ namespace net.narazaka.vrchat.avatar_parameters_exclusive_group.editor
                     var mode = driveCondition.FindPropertyRelative(nameof(DriveCondition.Mode));
                     rect.y += EditorGUIUtility.standardVerticalSpacing;
                     rect.height = EditorGUIUtility.singleLineHeight;
-                    EditorGUI.PropertyField(rect, driveCondition);
+                    EditorGUI.PropertyField(rect, driveCondition, T.Parameter.GUIContent);
                     rect.y += rect.height + EditorGUIUtility.standardVerticalSpacing;
                     if (mode.enumValueIndex < 2)
                     {
                         fallbackValue.floatValue = mode.enumValueIndex == 0 ? 0 : 1;
                         EditorGUI.BeginDisabledGroup(true);
-                        EditorGUI.Toggle(rect, fallbackValue.displayName, fallbackValue.floatValue != 0);
+                        EditorGUI.Toggle(rect, T.FallbackValue, fallbackValue.floatValue != 0);
                         EditorGUI.EndDisabledGroup();
                     }
                     else
                     {
-                        EditorGUI.PropertyField(rect, fallbackValue);
+                        EditorGUI.PropertyField(rect, fallbackValue, T.FallbackValue.GUIContent);
                     }
                 },
                 elementHeightCallback = (index) =>
@@ -55,9 +57,32 @@ namespace net.narazaka.vrchat.avatar_parameters_exclusive_group.editor
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
-            EditorGUILayout.PropertyField(LocalOnlyProperty);
+            EditorGUILayout.HelpBox(T.Description, MessageType.Info);
+            ShowDetail = EditorGUILayout.Foldout(ShowDetail, T.Detail);
+            if (ShowDetail)
+            {
+                EditorGUILayout.HelpBox(T.DetailDescription, MessageType.Info);
+            }
+            EditorGUILayout.PropertyField(LocalOnlyProperty, T.LocalOnly.GUIContent);
             ExclusiveParametersList.DoLayoutList();
             serializedObject.ApplyModifiedProperties();
+        }
+
+        static class T
+        {
+            public static istring Description = new istring(
+                "Set a pair of parameters that do not conflict at the same time. Parameters other than the one that caused the conflict will be automatically changed to the fallback value.",
+                "同時に成立しないパラメーターの組を設定します。競合を起こしたパラメーター以外がフォールバック値に自動的に変更されるようになります。"
+                );
+            public static istring Detail = new istring("Examples", "例");
+            public static istring DetailDescription = new istring(
+                "■Example: When a pair of A=ON and B=ON is set\n\nA=ON\n↓\nbecomes B=ON\n↓\n↓(automatic conflict resolution)\n↓\nswitched to A=OFF",
+                "■例: A=ONとB=ONの組を設定した時\n\nA=ONの状態\n↓\nB=ONになった！\n↓\n↓（自動で競合を解消）\n↓\nA=OFFに切り替わる"
+                );
+            public static istring LocalOnly = new istring("Local Only", "ローカルのみ");
+            public static istring ExclusiveParameters = new istring("Conflicting Parameter Sets", "競合するパラメータのセット");
+            public static istring Parameter = new istring("Parameter", "パラメータ");
+            public static istring FallbackValue = new istring("Fallback Value On Conflict", "競合時にフォールバックする値");
         }
     }
 }
